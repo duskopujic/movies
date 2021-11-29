eba44a6b760a78fdff3a92585a669a2c941319fa
pujicd <dusko.pujic@lanaco.com>
2021-11-29 12:41:44 +0100

    my first git

--- movies/src/main/java/com/lanaco/movies/MoviesApplication.java
+++ movies/src/main/java/com/lanaco/movies/MoviesApplication.java
@@ -1,4 +1,4 @@
-package com.lanaco.movies.Config;
+package com.lanaco.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
