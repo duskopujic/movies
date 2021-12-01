51fc0db8226f2fadddd6f0c3e8d03f3637b5bc27
pujicd <dusko.pujic@lanaco.com>
2021-12-01 10:27:34 +0100

    Swagger config

---
+++ movies/src/main/java/com/lanaco/movies/Config/SwaggerConfiguration.java
@@ -0,0 +1,30 @@
+package com.lanaco.movies.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket productApi(){
+    return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any()).build();
+            //.apiInfo(getApiInfo());
+    }
+   // private ApiInfo getApiInfo(){
+     //   return new ApiInfo("MovieApp - servisi", "api specifikacija aplikacije", "1.0.0", "", null, "", "", new ArrayList<>());
+    //}
+}
